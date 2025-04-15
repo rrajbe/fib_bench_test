b8bcd22bc5d8795484db33eb5002bdfc30f51af3
Rajkumar Ramasamy <rraj.be@gmail.com>
2025-04-15 17:42:54 +0400

    Test cargo bench

---
+++ benches/fibonacci_bench.rs
@@ -0,0 +1,16 @@
+#![feature(test)]
+
+extern crate test;
+
+use fib_bench_test::fibonacci;
+use test::Bencher;
+
+#[bench]
+fn bench_fibonacci_rec_ten(b: &mut Bencher) {
+    b.iter(|| fibonacci_rec(10));
+}
+
+#[bench]
+fn bench_fibonacci_loop_ten(b: &mut Bencher) {
+    b.iter(|| fibonacci_loop(10));
+}
